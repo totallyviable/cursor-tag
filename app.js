654598d53ec7e2b2cfb0e4d1e39f7f0ea85579d6
jake <jacob.bijani@gmail.com>
2015-08-02 15:45:44 -0400

    track clients in server. register tags via clicks.

--- app.js
+++ app.js
@@ -11,7 +11,19 @@ console.log('==================================================');
 
 app.use(express.static(__dirname + '/public'));
 
+var clients = {};
+
 io.on('connection', function (socket){
+    clients[socket.id] = socket;
+
+    socket.emit('player_count', {
+        count: Object.keys(clients).length
+    });
+
+    socket.broadcast.emit('player_count', {
+        count: Object.keys(clients).length
+    });
+
     socket.on('cursor_position', function(data){
         socket.volatile.broadcast.emit('cursor_position', {
             client_id: socket.id,
@@ -19,9 +31,25 @@ io.on('connection', function (socket){
         });
     });
 
+    socket.on('cursor_click', function(data){
+        var to_client = clients[data.client_id];
+
+        if (! to_client) return;
+
+        to_client.emit('cursor_tagged', {
+            from_client_id: socket.id
+        })
+    });
+
     socket.on('disconnect', function (){
         io.emit('cursor_disconnect', {
             client_id: socket.id
+        });
+
+        delete clients[socket.id];
+
+        socket.broadcast.emit('player_count', {
+            count: Object.keys(clients).length
         });
     });
 });
