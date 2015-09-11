f45307d9cd425c214bee513d1f0157cca185a405
jake <jacob.bijani@gmail.com>
2015-09-10 20:21:54 -0400

    add stathat trackers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var stathat = require('stathat');
 
 server.listen(process.env.PORT);
 
@@ -16,6 +17,14 @@ app.use(express.static(__dirname + '/public'));
 app.get('/about', function (req, res){
     res.sendFile(__dirname + '/public/about.html');
 });
+
+function count_stat(name, value){
+    return stathat.trackEZCount(process.env.STATHAT_KEY, "cursortag." + process.env.ENVIRONMENT + "." + name, value, function(status, json) {});
+}
+
+function value_stat(name, value){
+    return stathat.trackEZValue(process.env.STATHAT_KEY, "cursortag." + process.env.ENVIRONMENT + "." + name, value, function(status, json) {});
+}
 
 var active_clients = {};
 
@@ -30,6 +39,9 @@ io.on('connection', function (socket){
         socket.paused = false;
 
         io.emit('player_count', Object.keys(active_clients).length);
+
+        count_stat("activate_webcam", 1);
+        value_stat("player_count", Object.keys(active_clients).length);
     });
 
     socket.on('cursor_position', function(data){
@@ -61,6 +73,8 @@ io.on('connection', function (socket){
         if (! opponent_client) return;
 
         opponent_client.emit('receive_webcam_frame', data);
+
+        count_stat("webcam_frame", 1);
     });
 
     socket.on('paused', function(data){
@@ -81,5 +95,7 @@ io.on('connection', function (socket){
         if (active_clients[socket.id]) delete active_clients[socket.id];
 
         io.emit('player_count', Object.keys(active_clients).length);
+
+        value_stat("player_count", Object.keys(active_clients).length);
     });
 });
